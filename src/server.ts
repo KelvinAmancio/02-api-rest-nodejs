158c2b48e0ce06767b93e2e5213feb14204056b0
KelvinAmancio <kelvin013@gmail.com>
2024-03-11 23:10:39 -0300

    build: adapt system to bind to port on render

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT }).then(() => console.log('HTTP Server Running!'))
+const host = 'RENDER' in process.env ? '0.0.0.0' : 'localhost'
+
+app.listen({ port: env.PORT, host }).then(() => console.log('HTTP Server Running!'))
